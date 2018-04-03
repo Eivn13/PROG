31079463e87091f320d62d2b36054244100a9e30
Eivn13 <dominikpruzinsky@gmail.com>
2018-04-03 22:59:10 +0200

    adding comments for zad4

--- zad4.sh
+++ zad4.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 #EXECUTE WITH bash xxx.sh
-> ipcky.txt #reset data holder
 
-myAdapter=$(ifconfig | head -9 | grep "inet addr:" | cut -d ":" -f 2 | cut -d " " -f 1) #get ip
-ping_out=$(ping -c 3 $myAdapter)
-if [ $? = 0 ]
+
+myAdapter=$(ifconfig | head -9 | grep "inet addr:" | cut -d ":" -f 2 | cut -d " " -f 1) #get ip from ifconfig, cut string on : and then choose second substring, then cut on whitespace and get first substring
+ping_out=$(ping -c 3 $myAdapter) #ping myadapter ip with 3 packets
+if [ $? = 0 ]   #if result from last command was 0
 then
     echo "Machine is online"
 else
     echo "Machine is offline"
 fi
 
-gateWay=$(route | awk /'default/ { print $2 }')
-gw_ping_out=$(ping -c 3 $gateWay)
+gateWay=$(route | awk /'default/ { print $2 }') #call route, then get line with default and get second column
+gw_ping_out=$(ping -c 3 $gateWay) #ping gateway with 3 packets, im saving output to suppress it
 if [ $? = 0 ]
 then
     echo "Gateway is online"
@@ -20,20 +20,20 @@ else
     echo "Gateway is offline"
 fi
 
-dnses=$(cat /etc/resolv.conf | grep "nameserver" | awk /' / { print $2 }')
-read -a arr <<< $dnses
-for i in "${arr[@]}"
+dnses=$(cat /etc/resolv.conf | grep "nameserver" | awk /' / { print $2 }') #write out file resolv.conf, get lines with nameserver in it, awk out second column aka ip addr
+read -a arr <<< $dnses #send variable to read command and make array, one line, one element in array
+for i in "${arr[@]}" #travel through array
 do
-    dns=$(ping -c3 "${arr[@]}")
+    dns=$(ping -c3 "${arr[@]}") #ping, saving to var to suppress
     if [ $? = 0 ]
     then
-        echo "Dns server: "${arr[@]}" is online"
+        echo "Dns server: "${arr[@]}" is online" # outputting ip of DNS, because there might be more than one #clarity
     else
         echo "Dns server: "${arr[@]}" is offline"
     fi
 done
 
-google=$(nslookup google.com | grep 'Address: ' | awk '{ print $2}')
+google=$(nslookup google.com | grep 'Address: ' | awk '{ print $2}') #get network info from domain name, get line with address, get second "column"
 googleAvail=$(ping -c3 $google)
 if [ $? = 0 ]
 then
@@ -42,4 +42,4 @@ else
     echo "Google is down"
 fi
 
-echo "Script duration: $SECONDS sec"
+echo "Script duration: $SECONDS sec" #output runtime of script
