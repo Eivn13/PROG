1e458cfc26c0233c66b0baf82099d7c7295eea20
Eivn13 <33839991+Eivn13@users.noreply.github.com>
2018-04-11 15:48:48 +0200

    Update zad5.sh

--- zad5.sh
+++ zad5.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
+> aloha.txt
 ip=$(ifconfig | grep "inet addr:" | cut -d ":" -f2 | cut -d " " -f1 | head -1)
-echo "IPv4 addr NIC: $ip"
+echo "IPv4 addr NIC:   $ip" >> aloha.txt
 
 gw=$(route | grep "default" | awk '{print $2}')
-echo "IPv4 default gateway: $gw"
+echo "IPv4 default gateway: $gw" >> aloha.txt
 
 mask=$(ifconfig | grep "Mask" | cut -d ":" -f4 | head -1)
-echo "Network mask: $mask"
+echo "Network mask: $mask" >> aloha.txt
 
 dnses=$(cat /etc/resolv.conf | grep "nameserver" | awk /' / { print $2 }') #write out file resolv.conf, get lines with nameserver in it, awk out second column aka ip addr
 read -a arr <<< $dnses #send variable to read command and make array, one line, one element in array
 output="";
-for i in "${arr[@]}" #travel through array
+for i in "${!arr[@]}" #travel through array
 do
-    output="$output DNS address(es) : "${arr[@]}","
+    output="$output DNS address num$i: "${arr[$i]}","
 done
-echo ${output::-1}
+echo ${output::-1}  >> aloha.txt
 
-#1GiB = 1.074 GB, need to convert from GiB to GB
-hddSize=$(fdisk -l | grep "Disk /dev/sda" | cut -d "," -f1 | cut -d ":" -f2)
-hddSize=${hddSize::-3}
-answer=$(bc <<< "$hddSize*1.074")
-echo "Capacity of hdd: $hddSize GB"
+hddSize=$(df -H | grep /dev/sda1 | awk '{print $2}') #df -H prints sizes in powers of 1000
+hddSize=${hddSize::-1} #delete last character
+echo "Capacity of hdd:   $hddSize GB" >> aloha.txt
 
-freespace=$(df -h / | grep dev | awk '{print $4}')
-echo "Free space on / partition: ${freespace::-1} GB"
+freespace=$(df -h / | grep dev | awk '{print $4}') #df -h prints sizes in powers of 1024
+echo "Free space on / partition:   ${freespace::-1} GB" >> aloha.txt
 
-awk '/MemTotal/ {print "Total RAM: " $2/1024 " MiB"}' /proc/meminfo
-free | awk '/Mem/ {print "Usage of RAM: " $3/$2 * 100.0 "%"}'
-uname -r | awk '//{print "Version of kernel: " $1 }'
-cat /etc/os-release | grep "PRETTY_NAME" | cut -d "\"" -f2 | awk '//{print "Type of distribution and version: " $1 " " $2 " " $3}'
+awk '/MemTotal/ {print "Total RAM: " $2/1024 " MiB"}' /proc/meminfo   >> aloha.txt #call /proc/meminfo find line with MemTotal, then find second column and divide by 1024 to get MiB
+free | awk '/Mem/ {print "Usage of RAM: " $3/$2 * 100.0 "%"}'  >> aloha.txt #call command free, find line with Mem, divide column 3 with column 2 and multiply by 100.0
+uname -r | awk '//{print "Version of kernel: " $1 }'  >> aloha.txt
+cat /etc/os-release | grep "PRETTY_NAME" | cut -d "\"" -f2 | awk '//{print "Type of distribution and version: " $1 " " $2 " " $3}' >> aloha.txt #find line with PRETTY_NAME, cut line on " and choose second substring, then print column 1 2 and 3
+cat aloha.txt
